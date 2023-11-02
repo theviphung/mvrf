e672ecdb3279cb063c9f49af5803ec16033eaded
Milan-Lort <milanlort@outlook.com>
2023-11-01 19:26:24 -0700

    added another drop down menu improved spacing somewhat

--- app/javascript/dropdown.js
+++ app/javascript/dropdown.js
@@ -2,3 +2,33 @@
 function OpenFilters() {
     document.getElementById("filters").classList.toggle("show");
 }
+
+function OpenSortBy() {
+    document.getElementById("sorting").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches(".filters-button")) {
+        var dropdowns = document.getElementByClassName("filters-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains("show")) {
+                openDropdown.classList.remove("show");
+            }
+        }
+    }
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches(".sorting-button")) {
+        var dropdowns = document.getElementByClassName("sorting-methods");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains("show")) {
+                openDropdown.classList.remove("show");
+            }
+        }
+    }
+}
