4e763450d2d22d9b960df2abca30f3fdc2721fca
Milan-Lort <milanlort@outlook.com>
2023-11-10 14:37:54 -0800

    can now switch between restaurants in the view

--- app/javascript/switch-views.js
+++ app/javascript/switch-views.js
@@ -1,12 +1,22 @@
-function SwitchRestaurantView() {
-    var restaurants = document.getElementsByClassName("restaurant-view");
-    var i;
-    for (i = 0; i < restaurants.length; i++) {
-        var openRestaurant = restaurants[i];
-        openRestaurant.classList.toggle("show");
-    }
-
+function SwitchRestaurantView(id) {
+    
     if (document.getElementById("map").classList.contains("show")) {
         document.getElementById("map").classList.remove("show");
     }
+
+    if (document.getElementById(id).classList.contains("show")) {
+        document.getElementById(id).classList.remove("show");
+        document.getElementById("map").classList.add("show");
+    } else {
+        // remove the other shown elements
+        var restaurants = document.getElementsByClassName("restaurant-view");
+        for (var i = 0; i < restaurants.length; i++) {
+            var openRestaurant = restaurants[i];
+            if (openRestaurant.classList.contains("show")) {
+                openRestaurant.classList.remove("show");
+            }
+        }
+        document.getElementById(id).classList.add("show");
+    }
+
 }
