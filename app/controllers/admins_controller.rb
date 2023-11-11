044526695afc440673ed914ad13c8dbc7945413f
An <thn17@sfu.ca>
2023-11-10 20:35:28 -0800

    Add flash message for wrong username or password
    Still need to style the flash message

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -3,11 +3,13 @@ class AdminsController < ApplicationController
   end
 
   def create
-    admin = Admin.find_by(username: params[:username])
-    if admin && admin.authenticate(params[:password])
-      session[:admin_id] = admin.id
+    @admin = Admin.find_by(username: params[:username])
+    if @admin && @admin.authenticate(params[:password])
+      session[:admin_id] = @admin.id
+      flash[:notice] = "Logged in succesfully"
       redirect_to admin_dashboard_path
     else
+      flash[:alert] = "Invalid email or password"
       redirect_to '/admin-login'
     end
   end
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,3 +1,6 @@
 class Admin < ApplicationRecord
   has_secure_password
+
+  validates :username, uniqueness: true, presence: true
+  validates :password, presence: true
 end
