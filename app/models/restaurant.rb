1d3b6fc7aef1c4e8c8be1a2eae5f552081a528aa
Milan-Lort <milanlort@outlook.com>
2023-11-10 18:48:21 -0800

    Users can now search for restaurant by name it is very sensitive though will only work with correct spelling and capitalization

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,17 @@ class Restaurant < ApplicationRecord
     validates :name, presence: true
     validates :address, presence: true
     validates :description, presence: true, length: {minimum: 10}
+
+    def self.search(search)
+        if search
+            restaurant_return = Restaurant.find_by(name: search)
+            if restaurant_return
+                self.where(id: restaurant_return)
+            else
+                @restaurants = Restaurant.all
+            end
+        else
+            @restaurants = Restaurant.all
+        end
+    end
 end
