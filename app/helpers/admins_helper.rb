f4fe897aa69889f0254d5bfb5d86f79b61921bd8
An <thn17@sfu.ca>
2023-11-10 22:49:55 -0800

    Add some helper method to refine admin authentication logic

--- app/helpers/admins_helper.rb
+++ app/helpers/admins_helper.rb
@@ -1,2 +1,16 @@
 module AdminsHelper
+  #Return the current logged in admin
+  def current_admin
+    if @current_admin.nil?
+      @current_admin = Admin.find_by(id: session[:admin_id])
+    else
+      @current_admin
+    end
+  end
+
+  #Return true if admin is logged in. False otherwise
+  def admin_logged_in?
+    !session[:admin_id].nil?
+  end
+
 end
