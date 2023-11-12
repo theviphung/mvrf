d946ed306ecf6416c1c0366445ffbd30a26e7bcf
Milan-Lort <milanlort@outlook.com>
2023-11-11 19:05:44 -0800

    users can now search through restaurant cuisines addresses and descriptions

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -9,7 +9,13 @@ class Restaurant < ApplicationRecord
         if search
             self.where("lower(name) LIKE ? AND gluten_intolerance >= ? AND vegan >= ? AND vegetarian >= ? AND nut_allergy >= ? AND fish_allergy >= ? AND egg_allergy >= ? AND soy_allergy >= ? AND dairy_allergy >= ? AND kosher >= ? AND halal >= ?",
             "%#{search.downcase}%", gluten_req, vegan_req, vegetarian_req, nut_allergy_req, fish_allergy_req, egg_allergy_req, soy_allergy_req,
-            dairy_allergy_req, kosher_req, halal_req)
+            dairy_allergy_req, kosher_req, halal_req).or(self.where("lower(cuisine) LIKE ? AND gluten_intolerance >= ? AND vegan >= ? AND vegetarian >= ? AND nut_allergy >= ? AND fish_allergy >= ? AND egg_allergy >= ? AND soy_allergy >= ? AND dairy_allergy >= ? AND kosher >= ? AND halal >= ?",
+            "%#{search.downcase}%", gluten_req, vegan_req, vegetarian_req, nut_allergy_req, fish_allergy_req, egg_allergy_req, soy_allergy_req,
+            dairy_allergy_req, kosher_req, halal_req)).or(self.where("lower(address) LIKE ? AND gluten_intolerance >= ? AND vegan >= ? AND vegetarian >= ? AND nut_allergy >= ? AND fish_allergy >= ? AND egg_allergy >= ? AND soy_allergy >= ? AND dairy_allergy >= ? AND kosher >= ? AND halal >= ?",
+            "%#{search.downcase}%", gluten_req, vegan_req, vegetarian_req, nut_allergy_req, fish_allergy_req, egg_allergy_req, soy_allergy_req,
+            dairy_allergy_req, kosher_req, halal_req)).or(self.where("lower(description) LIKE ? AND gluten_intolerance >= ? AND vegan >= ? AND vegetarian >= ? AND nut_allergy >= ? AND fish_allergy >= ? AND egg_allergy >= ? AND soy_allergy >= ? AND dairy_allergy >= ? AND kosher >= ? AND halal >= ?",
+            "%#{search.downcase}%", gluten_req, vegan_req, vegetarian_req, nut_allergy_req, fish_allergy_req, egg_allergy_req, soy_allergy_req,
+            dairy_allergy_req, kosher_req, halal_req))
         else
             @restaurants = Restaurant.all
         end
